bf68de24a5f1dcac22d7d91f374cbaa596eae8a3
hsinpa <hsinpa@gmail.com>
2020-10-14 00:10:36 +0800

    Done with AR Image tracking

---
+++ XiangARUnity/Assets/ARTour/Script/ARItem/ARThemeAnchor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Expect.ARTour
+{
+    public class ARThemeAnchor : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform target;
+
+        public void SetLocalData(Vector3 pos, Quaternion rot, Vector3 scale) {
+            target.transform.localPosition = pos;
+            target.transform.localRotation = rot;
+            target.transform.localScale = scale;
+        } 
+    }
+}
--- XiangARUnity/Assets/ARTour/Script/ARItem/ARThemeItem.cs
+++ XiangARUnity/Assets/ARTour/Script/ARItem/ARThemeItem.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
-public class ARThemeItem : MonoBehaviour
+namespace Expect.ARTour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ARThemeItem : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private ARThemeAnchor[] ThemeItems;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Start()
+        {
+            SoundTextTourCtrl.OnDataSync += (OnARDataUpdate);
+        }
+
+        private void OnARDataUpdate(ARDataSync aRDataSync)
+        {
+            if (aRDataSync == null)
+            {
+                Debug.LogError("_ARTrackedImage is null");
+                return;
+            }
+
+            //Debug.Log("Reference Image name " + _ARTrackedImage.referenceImage.name);
+
+            foreach (ARThemeAnchor aRThemeAnchor in ThemeItems) {
+                ARDataSync.ARData data = aRDataSync.FindArData(aRThemeAnchor.name);
+
+                aRThemeAnchor.SetLocalData(data.position, data.rotation, data.scale);
+            }
+        }
+
+        public void UpdateThemeWorldPosData(string imageName, Vector3 worldPos, Quaternion rotation) {
+            var anchor = ThemeItems.FirstOrDefault<ARThemeAnchor>(x => x.name == imageName);
+            if (anchor == null) return;
+
+            anchor.transform.SetPositionAndRotation(worldPos, rotation);
+        }
+
+        public void ShowARTheme(string themeName) {
+            foreach (ARThemeAnchor themeItem in ThemeItems) {
+                    themeItem.gameObject.SetActive(themeItem.name == themeName);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            SoundTextTourCtrl.OnDataSync -= (OnARDataUpdate);
+        }
     }
 }
