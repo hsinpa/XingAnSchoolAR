6a17d7c557f0c440b59dc2cc8ec9108294fe567d
hsinpa <hsinpa@gmail.com>
2020-11-07 21:06:24 +0800

    Lock ar detection after period of scan

--- XiangARUnity/Assets/ARTour/Script/ARItem/ARThemeItem.cs
+++ XiangARUnity/Assets/ARTour/Script/ARItem/ARThemeItem.cs
@@ -12,13 +12,17 @@ namespace Expect.ARTour
         [SerializeField]
         private ARThemeAnchor[] ThemeItems;
 
-        private Vector3 recordPosition;
-        private float limitLengthDiff = 0.3f;
+        private string currentTheme;
+        private int lockCount, lockThreshold = 40;
+        private bool isLock;
+        private float lockResetTime = 10;
+        private float lockResetPending;
+
+        private enum CountType {PendCount =-2, StartCount = -1, Normal = 0 }
 
         void Start()
         {
             SoundTextTourCtrl.OnDataSync += (OnARDataUpdate);
-
         }
 
         private void OnARDataUpdate(ARDataSync aRDataSync)
@@ -40,19 +44,48 @@ namespace Expect.ARTour
 
         public void UpdateThemeWorldPosData(string imageName, Vector3 worldPos, Quaternion rotation) {
 
-            float diff = Vector3.Distance(worldPos, recordPosition);
-            recordPosition = worldPos;
-
-            if (diff > limitLengthDiff) {
+            if (LockPositionRoation(imageName))
                 return;
-            }
 
             var anchor = ThemeItems.FirstOrDefault<ARThemeAnchor>(x => x.name == imageName);
             if (anchor == null) return;
 
-            anchor.transform.rotation = Quaternion.Lerp(anchor.transform.rotation, rotation, 0.05f);
-            anchor.transform.position = Vector3.Lerp(anchor.transform.position, worldPos, 0.1f);
-            //anchor.transform.SetPositionAndRotation(worldPos, rotation);
+            //anchor.transform.rotation = Quaternion.Lerp(anchor.transform.rotation, rotation, 0.1f);
+            //anchor.transform.position = Vector3.Lerp(anchor.transform.position, worldPos, 0.2f);
+            anchor.transform.SetPositionAndRotation(worldPos, rotation);
+        }
+
+        private bool LockPositionRoation(string imageName) {
+            lockResetPending = Time.time + lockResetTime;
+
+            //Unlock
+            if (currentTheme != imageName) {
+                currentTheme = imageName;
+                lockCount = -1;
+            }
+
+            if (lockCount == (int)CountType.PendCount)
+            {
+                return true;
+            }
+
+            lockCount++;
+
+            if ((lockCount > lockThreshold)) {
+                isLock = !isLock;
+
+                //When lock, take longer time to unlock
+                lockCount = (isLock) ? (int)CountType.PendCount : (int)CountType.StartCount;
+            }
+
+            return isLock;
+        }
+
+        public void Update()
+        {
+            if (lockResetPending < Time.time) {
+                lockCount = (int)CountType.StartCount;
+            }            
         }
 
         public void ShowARTheme(string themeName) {
